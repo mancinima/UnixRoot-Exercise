7de1f33e72d79e648da0e557e5d5206cc8a34860
mancinima <mancinima@lnf.infn.it>
2023-06-05 17:39:43 +0200

    First pyroot exercise

---
+++ pyroot/include/Helper.py
@@ -0,0 +1,20 @@
+from IPython import get_ipython
+
+import numpy as np
+from numpy import random, dtype
+from array import array
+
+import math
+from math import floor
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from os import path, mkdir
+from tqdm import tqdm
+from pathlib import Path
+import sys
+from array import array
+
+import ROOT
+from ROOT import *
---
+++ pyroot/include/ROOTHelper.py
@@ -0,0 +1,8 @@
+import ROOT
+from ROOT import TCanvas, TFile, TPaveText, TMath, TRandom
+from ROOT import TF1, TH1, TH1F, TH2D, TH2F, TH3F, TPolyMarker3D, TGraphErrors, TBox, TLine, TAxis, TGaxis, TLegend
+from ROOT import TSpline5, TSpline3
+from ROOT import gROOT, gBenchmark, gRandom, gInterpreter, gPad, gStyle, gDirectory
+from ROOT import nullptr, kBlue, kRed, kGreen, kBlack, kMagenta, kOrange,kCyan,kViolet, kPink, kAzure
+
+#ROOT.gROOT.LoadMacro('./include/rootlogon.C')
